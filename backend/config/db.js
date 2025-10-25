b2e1a43d39d3b01b6e65e8fd446ed36ca1063b1a
Lokanath Sahoo <lokanath189@gmail.com>
2025-10-25 15:48:21 +0530

    db error resolve 3

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const mongoURI = process.env.MONGO_URI;
 
-const connectDB=async ()=>{
-    try {
-        await mongoose.connect(mongoURI,{});
-        console.log('✅ mongodb connected');
-    } catch (error) {
-        console.error('❌ mongodb connection failed:', error.message);
-        process.exit(1);
-    }
-}
-module.exports=connectDB;
+const mongoURI = process.env.MONGOURI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {});
+    console.log('✅ MongoDB connected');
+  } catch (error) {
+    console.error('❌ MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
