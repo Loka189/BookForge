5a81b2ae06c515254c6c30bb3c39990df412af85
Lokanath Sahoo <lokanath189@gmail.com>
2025-10-25 15:44:43 +0530

    db error resolve 2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const mongoURI = process.env.mongoURI;
+const mongoURI = process.env.MONGO_URI;
 
 const connectDB=async ()=>{
     try {
